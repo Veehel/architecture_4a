805f234748d077ee51fdd3db37a6d5e9e5898e94
marierouviere <marie.rouviere@laposte.net>
2020-02-26 11:04:27 +0100

    déplacements droite gauche

--- src/main/java/com/esiea/tp4A/domain/Direction.java
+++ src/main/java/com/esiea/tp4A/domain/Direction.java
@@ -3,4 +3,14 @@ package com.esiea.tp4A.domain;
 public enum Direction {
 
     NORTH, EAST, SOUTH, WEST;
+
+    public Direction left() {
+        int leftOrdinal = ordinal() -1;
+        return leftOrdinal == -1 ? WEST : Direction.values()[leftOrdinal];
+    }
+
+    public Direction right() {
+        int rightOrdinal = ordinal() +1;
+        return rightOrdinal == 4 ? NORTH : Direction.values()[rightOrdinal];
+    }
 }
