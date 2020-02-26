04be0e64a277498e375d3db1b755988a7e5e6469
delgrably <elgrably@et.esiea.fr>
2020-02-26 13:30:52 +0100

    Test MarsRover Position

--- src/main/java/com/esiea/tp4A/domain/MarsRoverMove.java
+++ src/main/java/com/esiea/tp4A/domain/MarsRoverMove.java
@@ -1,4 +1,25 @@
 package com.esiea.tp4A.domain;
 
 public class MarsRoverMove {
+
+    private Position position;
+
+    public MarsRoverMove(int x, int y, Direction direction){
+        position = Position.of(x,y,direction);
+    }
+
+
+    public Position move (String command){
+        if (command.isEmpty()){
+            return position;
+        }
+        char singleCommand = command.charAt(0);
+        if ('f' == singleCommand){
+            position = Position.of(position.getX(), position.getY() +1, position.getDirection());
+        } else if ('b' == singleCommand){
+            position = Position.of(position.getX(), position.getY() -1, position.getDirection());
+        }
+        return position;
+    }
+
 }
--- src/test/java/com/esiea/tp4A/domain/MarsRoverMoveTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsRoverMoveTest.java
@@ -1,5 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.esiea.tp4A.domain;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
 class MarsRoverMoveTest {
+    @Test
+    void initial_position_at_0() {
+        MarsRoverMove marsRover = new MarsRoverMove(0, 0, Direction.NORTH);
+        Position newPosition = marsRover.move("");
+        Assertions.assertThat(newPosition).isEqualTo(Position.of(0, 0, Direction.NORTH));
+    }
+
+    @Test
+    void initial_position_elsewhere() {
+        MarsRoverMove marsRover = new MarsRoverMove(32, 14, Direction.SOUTH);
+        Position newPosition = marsRover.move("");
+        Assertions.assertThat(newPosition).isEqualTo(Position.of(32, 14, Direction.SOUTH));
+    }
+
 
 }
