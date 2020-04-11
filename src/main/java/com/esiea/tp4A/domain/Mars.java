7c62e5ad5f257e63ca749f719f580856e883d234
danielgrably <danielgrably@yahoo.fr>
2020-04-12 00:31:09 +0200

    Correction Korekto

--- src/main/java/com/esiea/tp4A/domain/Mars.java
+++ src/main/java/com/esiea/tp4A/domain/Mars.java
@@ -7,12 +7,13 @@ import static java.lang.Math.round;
 
 public class Mars implements PlanetMap{
 
-    public final int xMax;
-    public final int xMin;
-    public final int yMax;
-    public final int yMin;
-    public final int size=100;
-    public final int [][] mars;
+    private final int xMax;
+    private final int xMin;
+    private final int yMax;
+    private final int yMin;
+    private  final int size=100;
+    private final int [][] mars;
+
     public Mars() {
         this.xMax = 50;
         this.xMin = -49;
@@ -20,22 +21,15 @@ public class Mars implements PlanetMap{
         this.yMin = -49;
         this.mars = new int[this.size][this.size];
         int x, y;
-
         for (y = 0; y < this.size; y++) {
-            for (x = 0; x < this.size; x++) {
-                this.mars[x][y] = 0;
-            }
+            for (x = 0; x < this.size; x++) { this.mars[x][y] = 0; }
         }
         int spawnObstacles= (int) round(0.16*this.size*this.size);
         for(int m=0;m<spawnObstacles;m++) {
             while (true) {
                 x = (int) (random() * this.size);
                 y = (int) (random() * this.size);
-
-                if (this.mars[x][y] == 0) {
-                    this.mars[x][y] = 1;
-                    break;
-                }
+                if (this.mars[x][y] == 0) {this.mars[x][y] = 1;break;}
             }
         }
     }
@@ -45,7 +39,6 @@ public class Mars implements PlanetMap{
         Set<Position> positions= new HashSet<>();
         int x,y;
         int offset=1-this.size/2;
-
         for(y=0;y<this.size;y++){
             for(x=0;x<this.size;x++){
                 if(mars[x][y]==1) {positions.add(Position.of(x+offset,y+offset,null));}
