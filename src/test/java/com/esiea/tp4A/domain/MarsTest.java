4babda82789cfed00dd9a34947582fc0aa5f22ee
IM94 <benchara.imane@gmail.com>
2020-04-12 13:40:53 +0200

    size corrections

--- src/test/java/com/esiea/tp4A/domain/MarsTest.java
+++ src/test/java/com/esiea/tp4A/domain/MarsTest.java
@@ -12,23 +12,17 @@ import static org.junit.jupiter.api.Assertions.*;
 class MarsTest {
     @Test
     void initMars() throws NoSuchFieldException,IllegalAccessException{
-        Mars mars=new Mars();
-        Field sField=mars.getClass().getDeclaredField("size");
+        Mars mars=new Mars(); Field sField=mars.getClass().getDeclaredField("size");
         int size=sField.getInt(mars);
         assertFalse(size<=0);
-
         Field mapField=mars.getClass().getField("mars");
         mapField.setAccessible(true);
         int[][] map= (int[][]) mapField.get(mars);
         assertEquals(size,map.length);
         assertEquals(size,map[0].length);
         int k=0;
-        for(int y=0;y<size;y++) {
-            for (int x = 0; x < size; x++) {
-                if (map[y][x] == 1) k++;
-            }
-        }
-        assertFalse(k+1<round(0.16*size*size));
+        for(int y=0;y<size;y++) { for (int x = 0; x < size; x++) { if (map[y][x] == 1) k++; }
+        }assertFalse(k+1<round(0.16*size*size));
     }
 
 	@Test
