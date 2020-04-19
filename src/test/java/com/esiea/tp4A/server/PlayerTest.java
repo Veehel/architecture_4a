08c5449b6478b3abcb6ff84337a2ad8e6b9e508e
danielgrably <danielgrably@yahoo.fr>
2020-04-19 18:55:12 +0200

    add test for server's class

--- src/test/java/com/esiea/tp4A/server/PlayerTest.java
+++ src/test/java/com/esiea/tp4A/server/PlayerTest.java
@@ -13,10 +13,18 @@ public class PlayerTest {
         Assertions.assertThat(p.getName()).isEqualTo("test");
     }
 
+    @Test
     void getRover() {
         MarsRoverMove marsRover = new MarsRoverMove(0, 0, Direction.NORTH);
         Player p = new Player("test");
         p.setRover(marsRover);
         Assertions.assertThat(p.getRover() == marsRover);
     }
+    
+    @Test
+    void new_player_status(){
+        Player player = new Player("name");
+        Assertions.assertThat(player.status).isEqualTo(PlayerStatus.alive);
+    }
+
 }
