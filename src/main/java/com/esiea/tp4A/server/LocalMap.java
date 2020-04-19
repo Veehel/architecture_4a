1f7e7de8a6e1e90e013698b76481bc95d523b39c
IM94 <benchara.imane@gmail.com>
2020-04-19 18:02:32 +0200

    add server

---
+++ src/main/java/com/esiea/tp4A/server/LocalMap.java
@@ -0,0 +1,36 @@
+package com.esiea.tp4A.server;
+
+import com.esiea.tp4A.domain.*;
+
+import java.util.HashSet;
+
+public class LocalMap {
+    static LocalMap localMap = new LocalMap();
+    private Mars map;
+    private HashSet<Player> players;
+
+    private LocalMap() {
+        map = new Mars();
+        players = new HashSet<Player>();
+    }
+
+    public static LocalMap get() {
+        return localMap;
+    }
+
+    void addPlayer(Player p) {
+        players.add(p);
+    }
+
+    Boolean hasPlayer(Player p) {
+        return players.contains(p);
+    }
+
+    void removePlayer(Player p) {
+        players.remove(p);
+    }
+
+    Mars getMap() {
+        return map;
+    }
+}
