80f2c2c513e396ef2db3cfbe733142e2ede35710
IM94 <benchara.imane@gmail.com>
2020-04-19 18:03:56 +0200

    add test server

---
+++ src/test/java/com/esiea/tp4A/server/LocalMapTest.java
@@ -0,0 +1,17 @@
+package com.esiea.tp4A.server;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LocalMapTest {
+    @Test
+    void getLocalMap() {
+        Assertions.assertThat(LocalMap.get()).isNotNull();
+    }
+
+    void players() {
+        Player p = new Player("test");
+        LocalMap.get().addPlayer(p);
+        Assertions.assertThat(LocalMap.get().hasPlayer(p)).isTrue();
+    }
+}
