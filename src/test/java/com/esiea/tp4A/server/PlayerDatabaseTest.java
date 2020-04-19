bb0b48abf28a1e7971159328608efd912d1acc32
danielgrably <danielgrably@yahoo.fr>
2020-04-19 21:12:54 +0200

    add server test

--- src/test/java/com/esiea/tp4A/server/PlayerDatabaseTest.java
+++ src/test/java/com/esiea/tp4A/server/PlayerDatabaseTest.java
@@ -5,10 +5,20 @@ import org.junit.jupiter.api.Test;
 
 public class PlayerDatabaseTest {
     @Test
-    void getPlayers() {
+    void find_Players() {
         PlayerDatabase db = PlayerDatabase.get();
         Player p = db.add("testPlayer");
         Assertions.assertThat(p.getName()).isEqualTo("testPlayer");
         Assertions.assertThat(db.find("testPlayer") == p);
     }
+
+    @Test
+    void null_Players() {
+        PlayerDatabase db = PlayerDatabase.get();
+        Player p = db.add("testPlayer");
+        Assertions.assertThat(p.getName()).isEqualTo("testPlayer");
+        Assertions.assertThat(db.find("nullPlayer") == null);
+    }
+
+
 }
