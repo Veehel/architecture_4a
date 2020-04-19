dc299965901072459ba6a5817ff4bd2273adc8b1
danielgrably <danielgrably@yahoo.fr>
2020-04-19 21:12:54 +0200

    add server test

--- src/test/java/com/esiea/tp4A/server/ServerTest.java
+++ src/test/java/com/esiea/tp4A/server/ServerTest.java
@@ -1,4 +1,14 @@
 package com.esiea.tp4A.server;
 
+import org.junit.jupiter.api.Test;
+
 public class ServerTest {
+
+    @Test
+    void main() throws InterruptedException {
+        Runnable main = () -> {
+            Server srv = new Server();
+            srv.Start();
+        };
+    }
 }
